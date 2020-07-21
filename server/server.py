46bc373d26e647c7d2726c88f8bcbfce9d47eec5
José Tomás Tocino <theom3ga@gmail.com>
2020-07-22 00:16:47 +0200

    Modify the server so that it serves the initial index file

--- server/server.py
+++ server/server.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, jsonify
+import os
+import sys
+
+from flask import Flask, request, jsonify, current_app
 from flask_cors import CORS
 
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from calculator.evaluator import evaluate
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 CORS(app)
 
 
 @app.route("/", methods=['GET'])
 def index():
+    return current_app.send_static_file('index.html')
+
+
+@app.route("/evaluate", methods=['GET'])
+def eval():
     expression = request.args.get('expression', '')
     notation = request.args.get('notation', 'standard')
 
